037df89ea1d80fa816f208621572af38b148bec5
arturMX <arturocr986@gmail.com>
2019-01-19 23:35:40 -0600

    Formulario con validaciones
    PROVEEDOR

--- web-app/src/scripts/modules/app.module.ts
+++ web-app/src/scripts/modules/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
  
 import { AppComponent } from '../components/app.component';
+import { FormModule } from './form.module';
  
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormModule
   ],
   declarations: [
     AppComponent
--- web-app/src/scripts/modules/form.module.ts
+++ web-app/src/scripts/modules/form.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
  import { FormComponent } from '../components/form.component';
+ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
  
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule 
   ],
   declarations: [
     FormComponent
   ],
+  exports: [
+    FormComponent
+  ],
   bootstrap: [ FormComponent ]
 })
-export class AppModule { }
+export class FormModule { }
