9703e3b05b35e8947059a1fcc7c0969c05b4966d
Bruno Bandala Pardo <brucebandark96@gmail.com>
2019-02-08 01:53:56 -0600

    Corrección en login (a evaluación)

--- web-app/src/scripts/components/login.component.ts
+++ web-app/src/scripts/components/login.component.ts
@@ -25,7 +25,6 @@ export class LoginComponent {
     public constructor(
         private router:Router, 
         private _sessionService : SessionService) {
-            // document.getElementById("demo-menu-lower-left").setAttribute("disabled", "false");
             if (screen.width<1024) 
                 this.showImage = false;
             else{
@@ -52,7 +51,6 @@ export class LoginComponent {
             localStorage.setItem("session",JSON.stringify(res.json()));
             ClientSessionService.isLoggedIn = true;
             ClientSessionService.username = username;
-            document.getElementById("demo-menu-lower-left").setAttribute("disabled", "false");
             this.router.navigate(['/home']);
           },(err:any)=>{
             let toast:any;
